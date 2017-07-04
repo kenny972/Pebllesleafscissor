ed7e70aa805abb701ee743db4df8d66fa22b7d86
Kenny <kennygenly@gmail.com>
2017-07-04 10:23:16 +0200

    Games is completed, it will appear on the screen

--- js/main.js
+++ js/main.js
@@ -1,5 +1,50 @@
 
-var yourChoice=prompt("chosen beetwen pebbles, leaf or scissor?");
+var joueur=prompt("pierre, feuille ou ciseau?");
+var ordi=Math.random();
+if (ordi < 0.34){
+  ordi = "pierre";
+}
 
-var choiceComputer = Math.random();
-console.log(choiceComputer);
+else if  (ordi <= 0.67){
+  ordi = "feuille";
+}
+
+else {
+  ordi = "ciseau";
+}
+alert("choix ordinateur " + " "+ ordi);
+
+
+function tirage (joueur,ordi){
+if (joueur === ordi){
+alert("egalite");
+}
+  else if (joueur === "ciseau"){
+   if (ordi === "pierre"){
+    return "ordi a gagne"
+   }
+    else {
+      return "joueur a gagne"
+    }
+  }
+  
+ else if (joueur === "feuille"){
+   if (ordi === "pierre"){
+    return "joueur a gagne"
+   }
+    else {
+      return "ordi a gagne"
+    }
+  }
+  
+ else if (joueur === "pierre"){
+   if (ordi === "ciseau"){
+    return "joueur a gagne"
+   }
+    else {
+      return "ordi a gagne"
+    }
+  }
+
+}
+alert(tirage(joueur,ordi));
